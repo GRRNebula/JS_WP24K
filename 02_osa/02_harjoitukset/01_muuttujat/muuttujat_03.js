2cd37074da732f101af019eca1d7bb4dd0069651
GRRNebula <65966485+GRRNebula@users.noreply.github.com>
2024-05-02 14:25:04 +0300

    tehty muuttujat_03.js

--- 02_osa/02_harjoitukset/01_muuttujat/muuttujat_03.js
+++ 02_osa/02_harjoitukset/01_muuttujat/muuttujat_03.js
@@ -2,3 +2,11 @@
 // Anna muuttujalle name arvoksi "Mikko".
 // Kopioi arvo muuttujasta name muuttujaan student.
 // Tulosta student-muuttujan arvo konsoliin (tulostus on "Mikko").
+
+let student = "";
+let name = "";
+
+name = "Maija";
+student = name;
+
+console.log(student);
